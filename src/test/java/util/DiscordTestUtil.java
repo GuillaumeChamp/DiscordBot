0782c6f68804f1ec2eaf0479b2c57c62828d11b3
guillaume <guillaume.champtoussel@ensea.fr>
2023-09-19 11:02:15 +0200

    setup workflow

--- src/test/java/util/DiscordTestUtil.java
+++ src/test/java/util/DiscordTestUtil.java
@@ -53,7 +53,7 @@ public class DiscordTestUtil {
             try {
                 api.awaitReady();
             } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+                throw new RuntimeException("Exception occurred during init" + e);
             }
         }
     }
